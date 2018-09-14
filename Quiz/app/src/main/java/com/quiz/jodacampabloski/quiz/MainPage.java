19b0f39907d9e1ecd5bfa72a040056de40b85437
Jose Danie <Jodacam2@gmail.com>
2018-09-14 10:54:47 +0200

    Añadido LayoutBasico

--- Quiz/app/src/main/java/com/quiz/jodacampabloski/quiz/MainPage.java
+++ Quiz/app/src/main/java/com/quiz/jodacampabloski/quiz/MainPage.java
@@ -1,7 +1,11 @@
 package com.quiz.jodacampabloski.quiz;
 
+import android.app.Application;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainPage extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+
+    }
+
+    public void StartGame(View v){
+        Intent nextActivty = new Intent(this,Game.class);
+        startActivity(nextActivty);
     }
 }
